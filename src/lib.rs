8cb91c3598bd16fd58e61e059c1acfac00379405
cgair <cgair@cgairdeMacBook-Pro.local>
2021-09-08 11:54:30 +0800

    Add benchmark and test it

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+// for benchmark
+#[inline]
+pub fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n-1) + fibonacci(n-2),
     }
 }
