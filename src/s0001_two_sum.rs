4ed1fb31cab83a306db4301803e1bd9d22ac641d
陈歌 <chenge@chengedeMacBook-Pro.local>
2022-02-11 12:53:41 +0800

    Add O(n) two_sum

--- src/s0001_two_sum.rs
+++ src/s0001_two_sum.rs
@@ -16,10 +16,32 @@
  * return [0, 1].
  *
  */
+use std::collections::HashMap;
 struct Solution {}
 
 impl Solution {
     pub fn two_sum(array: Vec<i32>, target: i32) -> Vec<i32>{
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        let size = array.len();
+        // for i in 0..size {
+        //     map.insert(array[i], i as i32);
+        // }
+        // for i in 0..size {
+        //     let minus = target - array[i];
+        //     if map.contains_key(&minus) {
+        //         let index = map.get(&minus).unwrap();
+        //         return vec![i as i32, *index];
+        //     }
+        // }
+        for i in 0..size {
+            let num = target - array[i];
+            if !map.contains_key(&num) {
+                map.insert(array[i], i as i32);
+            }else {
+                let index = map.get(&array[i]).unwrap();
+                return vec![i as i32, *index];
+            }
+        }
         vec![]
     }
 
@@ -41,12 +63,15 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_1() {
         let arr = vec![2, 7, 11, 15];
+        let arr2 = vec![2, 7, 11, 15];
         let target = 9;
         let ret = Solution::violent_two_sum(arr, target);
-        assert_eq!(ret, vec![0, 1])
+        assert_eq!(ret, vec![0, 1]);
+        let ret2 = Solution::violent_two_sum(arr2, target);
+        assert_eq!(ret2, vec![0, 1]);
+
     }
 }
