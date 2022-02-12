6844fc95a46f40a959db641063fe5db9d28b9203
cgair <cgair@cgairdeMacBook-Pro.local>
2022-02-12 15:19:05 +0800

    Add ListNode

---
+++ src/data_structure.rs
@@ -0,0 +1,16 @@
+use std::boxed::Box;
+
+#[derive(Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+    pub fn new(val: i32) -> Self {
+        ListNode {
+            val,
+            next: None
+        }
+    }
+}
