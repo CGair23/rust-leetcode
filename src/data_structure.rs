25086c2d7c1a9f5149dd288e01bb18af9f9b764d
cgair <cgair@cgairdeMacBook-Pro.local>
2022-02-13 14:52:35 +0800

    Add derive trait

--- src/data_structure.rs
+++ src/data_structure.rs
@@ -1,6 +1,7 @@
 use std::boxed::Box;
 
-#[derive(Debug)]
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>
