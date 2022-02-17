17d5ac08baae4c26f6094deeef724c09bc7d0556
CGair23 <chengeair23@sjtu.edu.cn>
2022-02-17 19:05:06 +0800

    Add s0003 longest_substring_without_repeating_characters

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod data_structure;
 mod s0001_two_sum;
 mod s0002_add_two_numbers;
 mod s0206_reverse_linked_list;
+mod s0003_longest_substring_without_repeating_characters;
 
 pub use s0206_reverse_linked_list::test_reverse_list;
 
--- src/s0003_longest_substring_without_repeating_characters.rs
+++ src/s0003_longest_substring_without_repeating_characters.rs
@@ -10,15 +10,41 @@
  * Explanation: The answer is "abc", with the length of 3.
  *
  */
+use std::collections::HashSet;
 
 pub struct Solution {}
 
 impl Solution {
     pub fn length_of_longest_substring(s: &str) -> u32 {
         let s_vec: Vec<char> = s.chars().collect();
-        let max_len = 0;
+        let len =  s_vec.len();
+        let (mut start, mut end, mut max_len) = (0, 0, 0);
+        let mut lookup = HashSet::new();
 
-        max_len
+        s_vec.iter().enumerate().for_each(|(i, ch)| {
+            while lookup.contains(ch) {
+                lookup.remove(&s_vec[start]);
+                start += 1;
+            }
+
+            lookup.insert(ch);
+            max_len = max_len.max(end - start + 1);
+            end += 1;
+        });
+
+            // ?
+            // fast_p += 1;
+            // curr_len += 1;
+            // if s_vec[slow_p] == s_vec[fast_p] {
+            //     if curr_len > max_len {
+            //         max_len = curr_len;
+            //     }
+            //     slow_p += 1;
+            //     fast_p = slow_p;
+            //     curr_len = 0;
+            // }
+
+        max_len as u32
     }
 }
 
@@ -27,7 +53,7 @@ mod tests {
     use super::*;
 
     #[test]
-    // char(&self)
+    // chars(&self)
     fn test_string_methods() {
         let word = "goodbye";
 
@@ -42,7 +68,25 @@ mod tests {
         assert_eq!(Some('o'), word.next());
     }
 
-    fn test_3() {
+    #[test]
+    // for_each<F>(self, f: F)
+    // where
+    // F: FnMut(Self::Item), 
+    fn test_iterator_trait() {
+        let v: Vec<_> =vec![0, 1, 2, 3, 4, 5];
+        v.iter().enumerate().for_each(|(i, v)| {
+            assert_eq!(0, *v);
+        });
+    }
 
+    #[test]
+    fn test_3() {
+        assert_eq!(3, Solution::length_of_longest_substring("abcabcbb"));
+        // assert_eq!(2, Solution::length_of_longest_substring("ab"));
+        // assert_eq!(Solution::length_of_longest_substring("bbbb"), 1);
+        assert_eq!(
+            Solution::length_of_longest_substring("pwwkew"),
+            3
+        );
     }
 }
